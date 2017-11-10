b9c431eaa0a110dd91453e5f993f66fa9eb4c4a0
soni <soni.rukmaji@gmail.com>
2017-11-11 01:30:25 +0530

    Adding difficulty buttons

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 var computerGuess;
 var userGuessLog = [];
 var attempts = 0;
+var maxGuesses = 10;
+
+function easyMode(){
+  maxGuesses = 10;
+  document.getElementById('easyBtn').className = 'activeButton';
+  document.getElementById('hardBtn').className = '';
+}
+
+function hardMode(){
+  maxGuesses = 5;
+  document.getElementById('hardBtn').className = 'activeButton';
+  document.getElementById('easyBtn').className = '';
+}
 
 function newGame() {
   window.location.reload();
@@ -9,6 +22,7 @@ function newGame() {
 function init() {
   computerGuess =Math.floor(Math.random() * 100 +1);
   //console.log(computerGuess);
+  document.getElementById('newGameButton').style.display = 'none';
 }
 
 function compareGuess(){
@@ -21,13 +35,25 @@ function compareGuess(){
   attempts++;
   document.getElementById('attempts').innerHTML = attempts;
 
-  if(userGuess > computerGuess){
-    document.getElementById('textOutput').innerHTML = 'Your guess is too high';
-    document.getElementById('inputBox').value = "";
-  } else if (userGuess < computerGuess) {
-    document.getElementById('textOutput').innerHTML = 'Your guess is too low';
-    document.getElementById('inputBox').value = "";
-} else {
-  document.getElementById('textOutput').innerHTML = 'Correct!';
-}
+  if(userGuessLog.length < maxGuesses){
+    if(userGuess > computerGuess){
+      document.getElementById('textOutput').innerHTML = 'Your guess is too high';
+      document.getElementById('inputBox').value = "";
+    } else if (userGuess < computerGuess) {
+      document.getElementById('textOutput').innerHTML = 'Your guess is too low';
+      document.getElementById('inputBox').value = "";
+    } else {
+      document.getElementById('textOutput').innerHTML = 'Correct!';
+    }
+  } else {
+    if(userGuess > computerGuess){
+      document.getElementById('textOutput').innerHTML = 'You lose!';
+    } else if(userGuess < computerGuess){
+      document.getElementById('textOutput').innerHTML = 'You lose!';
+    } else {
+      document.getElementById('textOutput').innerHTML = 'Correct!';
+    }
+
+  }
+
 }
